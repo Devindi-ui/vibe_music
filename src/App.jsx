36f81c0d4d159523ce8e15d1655f04b588e219e5
Devindi-u <panchalidevindi@gmail.com>
2025-10-01 16:16:10 +0530

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Devindi Wijesekara</h1>
+    </>
+  )
+}
+
+export default App
