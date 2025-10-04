6e962fb7e824a26592d93aa22461b89dc98941d3
Devindi-u <panchalidevindi@gmail.com>
2025-10-04 20:50:41 +0530

    header, themeToggle and loadingSpinner css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import Header from './components/common/Header'
 import LoadingSpinner from './components/common/LoadingSpinner'
+import "./styles/globals.css";
+import "./styles/theme.css";
+import "./styles/components.css";
 
 function App() {
 
